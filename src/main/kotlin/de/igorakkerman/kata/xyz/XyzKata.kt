90e7f6c8c70e647b8e841bc3e6db8269d35fa0ae
Igor Akkerman <codingchallenge@igorakkerman.de>
2019-06-10 15:47:31 +0200

    Algorithm and tests implemneted

--- src/main/kotlin/de/igorakkerman/kata/xyz/XyzKata.kt
+++ src/main/kotlin/de/igorakkerman/kata/xyz/XyzKata.kt
@@ -1,8 +1,42 @@
 package de.igorakkerman.kata.xyz
 
+import kotlin.math.pow
+
 class XyzKata {
 
-    fun solution(A: IntArray): Int {
-        return 0
+    fun sameCount(a: Array<IntArray>): Int {
+        return a.map { row ->
+            val first = row[0]
+            if (row.all { it == first }) 1 else 0
+        }
+            .reduce { x, y -> x + y }
     }
+
+    infix fun Int.`**`(exponent: Int): Int =
+        toDouble().pow(exponent).toInt()
+
+    fun solution(a: Array<IntArray>): Int {
+
+        val rows = asNumbers(a)
+
+        val allZeros = 0
+        val allOnes = allOnes(bitsCount = a[0].size)
+
+        return (allZeros..allOnes)
+            .map { mask ->
+                rows.map { row -> row xor mask }
+                    .filter { row -> row.equals(allZeros) || row.equals(allOnes) }
+                    .count()
+            }
+            .max()!!
+    }
+
+    internal fun asNumbers(a: Array<IntArray>): List<Int> {
+        return a.map { row ->
+            row.reduce { x, y -> x * 2 + y }
+        }
+    }
+
+    internal fun allOnes(bitsCount: Int): Int =
+        (2.toDouble().pow(bitsCount) - 1).toInt()
 }
--- src/test/kotlin/de/igorakkerman/kata/xyz/XyzKataTest.kt
+++ src/test/kotlin/de/igorakkerman/kata/xyz/XyzKataTest.kt
@@ -1,6 +1,7 @@
 package de.igorakkerman.kata.xyz
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
 internal class XyzKataTest {
@@ -8,20 +9,222 @@ internal class XyzKataTest {
     val kata = XyzKata()
 
     @Test
-    internal fun empty() {
-        assertThat(kata.solution(IntArray(0)))
-                .isEqualTo(0)
+    internal fun `1x1`() {
+        assertThat(kata.solution(arrayOf(intArrayOf(1))))
+            .isEqualTo(1)
     }
 
     @Test
-    internal fun `single char`() {
-        assertThat(kata.solution(intArrayOf(1)))
-                .isEqualTo(0)
+    internal fun `2x1 (0 0)`() {
+        val a = arrayOf(
+                intArrayOf(0),
+                intArrayOf(0)
+        )
+
+        assertThat(kata.solution(a))
+            .isEqualTo(2)
     }
 
     @Test
-    internal fun `sequence of numbers`() {
-        assertThat(kata.solution(intArrayOf(1, 2, 3, 4, 5)))
+    internal fun `2x1 (1 1)`() {
+        val a = arrayOf(
+                intArrayOf(1),
+                intArrayOf(1)
+        )
+        assertThat(kata.solution(a))
+            .isEqualTo(2)
+    }
+
+    @Test
+    internal fun `2x1 (0 1)`() {
+        val a = arrayOf(
+                intArrayOf(0),
+                intArrayOf(1)
+        )
+        assertThat(kata.solution(a))
+            .isEqualTo(2)
+    }
+
+    @Test
+    internal fun `2x1 (1 0)`() {
+        val a = arrayOf(
+                intArrayOf(1),
+                intArrayOf(0)
+        )
+        assertThat(kata.solution(a))
+            .isEqualTo(2)
+    }
+
+    @Test
+    internal fun `1x2 (0 0)`() {
+        val a = arrayOf(
+                intArrayOf(0, 0)
+        )
+        assertThat(kata.solution(a))
+            .isEqualTo(1)
+    }
+
+    @Test
+    internal fun `1x2 (1 1)`() {
+        val a = arrayOf(
+                intArrayOf(1, 1)
+        )
+        assertThat(kata.solution(a))
+            .isEqualTo(1)
+    }
+
+    @Test
+    internal fun `1x2 (0 1)`() {
+        val a = arrayOf(
+                intArrayOf(0, 1)
+        )
+        assertThat(kata.solution(a))
+            .isEqualTo(1)
+    }
+
+    @Test
+    internal fun `1x2 (1 0)`() {
+        val a = arrayOf(
+                intArrayOf(1, 0)
+        )
+        assertThat(kata.solution(a))
+            .isEqualTo(1)
+    }
+
+    @Test
+    internal fun `3x4 - swap col 2,3 - rows 2,3`() {
+        val a = arrayOf(
+                intArrayOf(1, 0, 0, 1),
+                intArrayOf(0, 1, 1, 0),
+                intArrayOf(1, 1, 0, 0)
+        )
+        assertThat(kata.solution(a))
+            .isEqualTo(2)
+    }
+
+    @Test
+    internal fun `3x4 - swap 2,3,4 - row 1`() {
+        val a = arrayOf(
+                intArrayOf(1, 0, 0, 0),
+                intArrayOf(0, 1, 0, 0),
+                intArrayOf(0, 0, 1, 0)
+        )
+        assertThat(kata.solution(a))
+            .isEqualTo(1)
+    }
+
+    @Test
+    internal fun `3x4 - swap 1,3,4 - rows 2,3`() {
+        val a = arrayOf(
+                intArrayOf(0, 0, 0, 0),
+                intArrayOf(0, 1, 0, 0),
+                intArrayOf(1, 0, 1, 1)
+        )
+        assertThat(kata.solution(a))
+            .isEqualTo(2)
+    }
+
+    @Test
+    internal fun `4x4 - swap 1,2,3,4 - rows 1,2,3,4`() {
+        val a = arrayOf(
+                intArrayOf(0, 1, 0, 1),
+                intArrayOf(1, 0, 1, 0),
+                intArrayOf(0, 1, 0, 1),
+                intArrayOf(1, 0, 1, 0)
+        )
+        assertThat(kata.solution(a))
+            .isEqualTo(4)
+    }
+
+    @Nested
+    inner class AllOnes {
+        @Test
+        internal fun `allOnes 0`() {
+            assertThat(kata.allOnes(0))
                 .isEqualTo(0)
+        }
+
+        @Test
+        internal fun `allOnes 1`() {
+            assertThat(kata.allOnes(1))
+                .isEqualTo(1)
+        }
+
+        @Test
+        internal fun `allOnes 2`() {
+            assertThat(kata.allOnes(2))
+                .isEqualTo(3)
+        }
+    }
+
+    @Nested
+    inner class AsNumbers {
+
+        @Test
+        internal fun `asNumbers (0)`() {
+            val a = arrayOf(
+                    intArrayOf(0)
+            )
+            assertThat(kata.asNumbers(a))
+                .containsExactly(0)
+        }
+
+        @Test
+        internal fun `asNumbers (0, 1)`() {
+            val a = arrayOf(
+                    intArrayOf(0, 1)
+            )
+            assertThat(kata.asNumbers(a))
+                .containsExactly(1)
+        }
+
+        @Test
+        internal fun `asNumbers (1, 0)`() {
+            val a = arrayOf(
+                    intArrayOf(1, 0)
+            )
+            assertThat(kata.asNumbers(a))
+                .containsExactly(2)
+        }
+
+        @Test
+        internal fun `asNumbers (1, 1, 0)`() {
+            val a = arrayOf(
+                    intArrayOf(1, 1, 0)
+            )
+            assertThat(kata.asNumbers(a))
+                .containsExactly(6)
+        }
+
+        @Test
+        internal fun `asNumbers ((0), (1))`() {
+            val a = arrayOf(
+                    intArrayOf(0),
+                    intArrayOf(1)
+            )
+            assertThat(kata.asNumbers(a))
+                .containsExactly(0, 1)
+        }
+
+        @Test
+        internal fun `asNumbers ((0,1), (1,0))`() {
+            val a = arrayOf(
+                    intArrayOf(0, 1),
+                    intArrayOf(1, 0)
+            )
+            assertThat(kata.asNumbers(a))
+                .containsExactly(1, 2)
+        }
+
+        @Test
+        internal fun `asNumbers ((1, 1, 0),(0,1,0))`() {
+            val a = arrayOf(
+                    intArrayOf(1, 1, 0),
+                    intArrayOf(0, 1, 0)
+            )
+            assertThat(kata.asNumbers(a))
+                .containsExactly(6, 2)
+        }
+
     }
 }
