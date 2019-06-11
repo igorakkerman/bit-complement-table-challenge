c960e0df6a35fd287a1ab4f9d958b884c14ca975
Igor Akkerman <codingchallenge@igorakkerman.de>
2019-06-11 02:34:26 +0200

    Operating on lists (containing the row values) instead of BigDecimals

--- src/main/kotlin/de/igorakkerman/kata/xyz/XyzKata.kt
+++ src/main/kotlin/de/igorakkerman/kata/xyz/XyzKata.kt
@@ -1,28 +1,15 @@
 package de.igorakkerman.kata.xyz
 
-import java.math.BigInteger
-import java.math.BigInteger.ONE
-
 class XyzKata {
-
-    private val TWO = BigInteger.valueOf(2)
-
     fun solution(a: Array<IntArray>): Int {
 
-        val counts = mutableMapOf<BigInteger, Int>()
-        val allOnes = TWO.pow(a[0].size) - ONE
+        val counts = mutableMapOf<List<Int>, Int>()
 
-        for (rowValue in asNumbers(a))
-            counts[rowValue] = (counts[rowValue] ?: 0) + 1
-
-        return counts.asSequence().map { it.value + (counts[allOnes.xor(it.key)] ?: 0) }.max()!!
-    }
-
-    internal fun asNumbers(a: Array<IntArray>): List<BigInteger> {
-        return a.map { rowCells ->
-            rowCells
-                .map { BigInteger.valueOf(it.toLong()) }
-                .reduce { x: BigInteger, y: BigInteger -> TWO.times(x).plus(y) }
+        a.forEach {
+            val l = it.toList()
+            counts[l] = (counts[l] ?: 0) + 1
         }
+
+        return counts.asSequence().map { it.value + (counts[it.key.map { bit -> bit xor 1 }] ?: 0) }.max()!!
     }
 }
